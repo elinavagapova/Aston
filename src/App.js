99c364c176480ffe0bd487b3eb3b8ddf93fb935d
Elina Vagapova <vagapovaelina98@gmail.com>
2023-09-14 11:06:24 +0300

    Подключение redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import {Home} from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-          <header/>
-          <main>
-              <Routes>
-                  <Route path='/' element={<Home/>}/>
-              </Routes>
-          </main>
-          <footer/>
-      </div>
-    </Router>
+    <div className='app'>
+      <header />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </main>
+      <footer />
+    </div>
   );
 }
 
