c98b7d32a77e7ebedb74b8e4952108fe79ad3065
Elina Vagapova <vagapovaelina98@gmail.com>
2023-09-17 19:03:22 +0300

    Мелкие правки

--- src/components/singleComic/SingleComic.js
+++ src/components/singleComic/SingleComic.js
@@ -1,6 +1,5 @@
 import { Link, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 
 import { Spinner } from '../spinner/Spinner';
 import { ErrorMessage } from '../errorMessage/ErrorMessage';
@@ -24,7 +23,20 @@ export function SingleComic() {
 
   const errorMessage = error ? <ErrorMessage /> : null;
   const spinner = loading ? <Spinner /> : null;
-  const content = !(loading || error || !comic) ? <View comic={comic} /> : null;
+  const content = !(loading || error || !comic) && (
+    <div className='single-comic'>
+      <img src={comic.thumbnail} alt={comic.title} className='single-comic__img' />
+      <div className='single-comic__info'>
+        <h2 className='single-comic__name'>{comic.title}</h2>
+        <p className='single-comic__descr'>{comic.description}</p>
+        <p className='single-comic__descr'>{comic.page}</p>
+        <div className='single-comic__price'>{comic.price}</div>
+      </div>
+      <Link to='/' className='single-comic__back'>
+        Вернуться назад
+      </Link>
+    </div>
+  );
 
   return (
     <>
@@ -34,26 +46,3 @@ export function SingleComic() {
     </>
   );
 }
-
-function View({ comic }) {
-  const { title, description, thumbnail, page, price } = comic;
-
-  return (
-    <div className='single-comic'>
-      <img src={thumbnail} alt={title} className='single-comic__img' />
-      <div className='single-comic__info'>
-        <h2 className='single-comic__name'>{title}</h2>
-        <p className='single-comic__descr'>{description}</p>
-        <p className='single-comic__descr'>{page}</p>
-        <div className='single-comic__price'>{price}</div>
-      </div>
-      <Link to='/' className='single-comic__back'>
-        Вернуться назад
-      </Link>
-    </div>
-  );
-}
-
-View.propTypes = {
-  comic: PropTypes.objectOf(PropTypes.string).isRequired,
-};
